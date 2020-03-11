7a187f4b0e2f44164aa96cbfcf3e950376136a23
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2020-03-12 01:57:11 +1100

    Darkness and confusion

--- opencv/DisplayImage.cpp
+++ opencv/DisplayImage.cpp
@@ -9,6 +9,8 @@ extern "C" {
     #include <libavformat/avformat.h>
     #include <libavcodec/avcodec.h>
     #include <libavutil/buffer.h>
+    #include <libavutil/hwcontext_opencl.h>
+    #include <libavutil/pixdesc.h>
     #include <va/va_drm.h>
 }
 
@@ -214,8 +216,117 @@ int process_frame(IoContext *ioContext, AVFrame *frame) {
     if (ret) {
         fprintf(stderr, "Failed to map hardware frames: %s\n", errString(ret));
     }
+
+    fprintf(stderr, "frame format: %s\n", av_get_pix_fmt_name((AVPixelFormat) frame->format));
+    fprintf(stderr, "ocl_frame format: %s\n", av_get_pix_fmt_name((AVPixelFormat) ocl_frame->format));
+    fprintf(stderr, "stack: %p\n", &ret);
+    fprintf(stderr, "data: %p %p %p\n", ocl_frame->data[0], ocl_frame->data[1], ocl_frame->data[2]);
+
+    UMat frame_mat;
+    cl_mem luma = (cl_mem) ocl_frame->data[0];
+    // cl_mem chroma = (cl_mem) ocl_frame->data[1];
+    ocl::convertFromImage(luma, frame_mat);
+
     av_frame_unref(ocl_frame);
     return ret;
+}
+
+int init_opencv_opencl_from_hwctx(AVBufferRef *ocl_device_ctx) {
+    int ret = 0;
+    AVHWDeviceContext *ocl_hw_device_ctx;
+    AVOpenCLDeviceContext *ocl_device_ocl_ctx;
+    ocl_hw_device_ctx = (AVHWDeviceContext *) ocl_device_ctx->data;
+    ocl_device_ocl_ctx = (AVOpenCLDeviceContext *) ocl_hw_device_ctx->hwctx;
+    cl_context_properties *props = NULL;
+    cl_platform_id platform = NULL;
+    char *platform_name = NULL;
+    size_t param_value_size = 0;
+    ret = clGetContextInfo(
+        ocl_device_ocl_ctx->context,
+        CL_CONTEXT_PROPERTIES,
+        0,
+        NULL,
+        &param_value_size
+    );
+    if (ret != CL_SUCCESS) {
+        std::cerr << "clGetContextInfo failed to get props size\n";
+        return ret;
+    }
+    if (param_value_size == 0) {
+        std::cerr << "clGetContextInfo returned size 0\n";
+        return 1;
+    }
+    props = (cl_context_properties *) malloc(param_value_size);
+    if (props == NULL) {
+        std::cerr << "Failed to alloc props 0\n";
+        return AVERROR(ENOMEM);
+    }
+    ret = clGetContextInfo(
+        ocl_device_ocl_ctx->context,
+        CL_CONTEXT_PROPERTIES,
+        param_value_size,
+        props,
+        NULL
+    );
+    if (ret != CL_SUCCESS) {
+        std::cerr << "clGetContextInfo failed\n";
+        return ret;
+    }
+    for (int i = 0; props[i] != 0; i = i + 2) {
+        if (props[i] == CL_CONTEXT_PLATFORM) {
+            platform = (cl_platform_id) props[i + 1];
+        }
+    }
+    if (platform == NULL) {
+        std::cerr << "Failed to find platform in cl context props\n";
+        return 1;
+    }
+
+    ret = clGetPlatformInfo(
+        platform,
+        CL_PLATFORM_NAME,
+        0,
+        NULL,
+        &param_value_size
+    );
+
+    if (ret != CL_SUCCESS) {
+        std::cerr << "clGetPlatformInfo failed to get platform name size\n";
+        return ret;
+    }
+    if (param_value_size == 0) {
+        std::cerr << "clGetPlatformInfo returned 0 size for name\n";
+        return 1;
+    }
+    platform_name = (char *) malloc(param_value_size);
+    if (platform_name == NULL) {
+        std::cerr << "Failed to malloc platform_name\n";
+        return AVERROR(ENOMEM);
+    }
+    ret = clGetPlatformInfo(
+        platform,
+        CL_PLATFORM_NAME,
+        param_value_size,
+        platform_name,
+        NULL
+    );
+    if (ret != CL_SUCCESS) {
+        std::cerr << "clGetPlatformInfo failed\n";
+        return ret;
+    }
+
+    std::cerr << "Initialising OpenCV OpenCL context with platform \"" <<
+        platform_name <<
+        "\"\n";
+
+    ocl::Context::getDefault(false);
+    ocl::attachContext(
+        platform_name,
+        platform,
+        ocl_device_ocl_ctx->context,
+        ocl_device_ocl_ctx->device_id
+    );
+    return 0;
 }
 
 int main (int argc, char* argv[])
@@ -253,6 +364,12 @@ int main (int argc, char* argv[])
             "Failed to map VAAPI device to OpenCL device. Error code: %s\n",
             errString(ret)
         );
+        return ret;
+    }
+
+    ret = init_opencv_opencl_from_hwctx(ioContext->ocl_device_ctx);
+    if (ret) {
+        std::cerr << "Failed to initialise OpenCV OpenCL from libavcodec hw ctx\n";
         return ret;
     }
 
