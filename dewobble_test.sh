69303bb597e606ad89a14a900f584da865585c81
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2021-04-21 23:26:51 +1000

    dewobble test: better quality for AMD

--- dewobble_test.sh
+++ dewobble_test.sh
@@ -15,61 +15,34 @@ FFMPEG=~/code/oss/FFmpeg/ffmpeg
 
 if [ $GPU = "intel" ]; then
 	INPUT_FLAGS=(
-		-init_hw_device
-		vaapi=intel_vaapi:/dev/dri/renderD128
-		-hwaccel
-		vaapi
-		-hwaccel_device
-		intel_vaapi
-		-hwaccel_output_format
-		vaapi
-		-init_hw_device
-		opencl=intel_opencl@intel_vaapi
-		-filter_hw_device
-		intel_opencl
+		-init_hw_device vaapi=intel_vaapi:/dev/dri/renderD128
+		-hwaccel vaapi -hwaccel_device intel_vaapi -hwaccel_output_format vaapi
+		-init_hw_device opencl=intel_opencl@intel_vaapi
+		-filter_hw_device intel_opencl
 	)
 	OUTPUT_FLAGS=(
-		-vf
-		"hwmap,$FILTER,hwmap=derive_device=vaapi:reverse=1"
+		-vf "hwmap,$FILTER,hwmap=derive_device=vaapi:reverse=1"
 		-c:v h264_vaapi
 	)
 elif [ $GPU = "nvidia" ]; then
 	INPUT_FLAGS=(
-		-init_hw_device
-		opencl=nvidia_opencl:1.0
-		-filter_hw_device
-		nvidia_opencl
-		-hwaccel
-		nvdec
+		-init_hw_device opencl=nvidia_opencl:1.0 -filter_hw_device nvidia_opencl
+		-hwaccel nvdec
 	)
 	OUTPUT_FLAGS=(
-		-vf
-		"hwupload,$FILTER,hwdownload,format=nv12"
-		-c:v
-		h264_nvenc
-		-qp
-		19
+		-vf "hwupload,$FILTER,hwdownload,format=nv12"
+		-c:v h264_nvenc -qp 19
 	)
 elif [ $GPU = "amd" ]; then
 	LIBVA_DRIVER_NAME=radeonsi
 	VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/amd_pro_x86_64.icd
 	INPUT_FLAGS=(
-		-init_hw_device
-		opencl=amd_opencl:2.0
-		-filter_hw_device
-		amd_opencl
-		-hwaccel
-		vaapi
-		-hwaccel_device
-		/dev/dri/renderD129
+		-init_hw_device opencl=amd_opencl:2.0 -filter_hw_device amd_opencl
+		-hwaccel vaapi -hwaccel_device /dev/dri/renderD129
 	)
 	OUTPUT_FLAGS=(
-		-vf
-		"hwupload,$FILTER,hwdownload,format=nv12"
-		-c:v
-		h264_amf
-		-qp
-		19
+		-vf "hwupload,$FILTER,hwdownload,format=nv12"
+		-c:v h264_amf -qp_i 21 -qp_b 21 -qp_p 21 -quality quality
 	)
 else
 	echo Unsupported GPU: $GPU
