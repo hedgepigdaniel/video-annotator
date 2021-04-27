013144b9a21614c2dc9e0843f41c85f9c793d7d6
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2021-04-27 22:22:07 +1000

    add script to compare stabilization filters

--- dewobble_test.sh
+++ dewobble_test.sh
@@ -21,10 +21,32 @@ if [ $GPU = "intel" ]; then
 	INPUT_FLAGS=(
 		-init_hw_device vaapi=intel_vaapi:,driver=iHD,kernel_driver=i915
 		-hwaccel vaapi -hwaccel_device intel_vaapi -hwaccel_output_format vaapi
+		-init_hw_device opencl=intel_opencl@intel_vaapi -filter_hw_device intel_opencl
 	)
 	OUTPUT_FLAGS=(
-		-vf "hwmap=derive_device=opencl,$FILTER,hwmap=reverse=1"
+		-vf "hwmap,$FILTER,hwmap=derive_device=vaapi:reverse=1"
 		-c:v h264_vaapi
+	)
+elif [ $GPU = "intelcomp" ]; then
+	INPUT_FLAGS=(
+		-init_hw_device vaapi=intel_vaapi:,driver=iHD,kernel_driver=i915
+		-hwaccel vaapi -hwaccel_device intel_vaapi
+		-init_hw_device opencl=intel_opencl@intel_vaapi -filter_hw_device intel_opencl
+	)
+	IN_CAMERA="in_p=fish:in_fl=943"
+	OUT_CAMERA="out_p=fish:out_fl=943"
+	THIRD_WIDTH=640
+	SMOOTHING=90
+	OUTPUT_FLAGS=(
+		-filter_complex
+"[0:v]hwupload,dewobble_opencl=$IN_CAMERA:$OUT_CAMERA:stab=none,hwdownload,format=nv12[base];"\
+"[0:v]hwupload,dewobble_opencl=$IN_CAMERA:out_w=$THIRD_WIDTH:$OUT_CAMERA:out_fx=$(($THIRD_WIDTH * 3 / 2)):stab=sg:stab_r=$SMOOTHING,hwdownload,format=nv12,format=yuv420p[left];"\
+"[0:v]vidstabtransform=smoothing=$SMOOTHING:optzoom=0:crop=black,format=nv12,hwupload,dewobble_opencl=$IN_CAMERA:out_w=$THIRD_WIDTH:$OUT_CAMERA:out_fx=$((-$THIRD_WIDTH * 1 / 2)):stab=none,hwdownload,format=nv12,format=yuv420p[right];"\
+"[base][left]overlay[base2];"\
+"[base2][right]overlay=x=$(($THIRD_WIDTH * 2))[out]"
+		-map "[out]"
+		-map "0:a"
+		-c:v h264_nvenc -qp 19
 	)
 elif [ $GPU = "nvidia" ]; then
 	INPUT_FLAGS=(
@@ -50,5 +72,10 @@ else
 	echo Unsupported GPU: $GPU
 fi
 
+if [ $GPU = "intelcomp" ]; then
+	"$FFMPEG" "${INPUT_FLAGS[@]}" -i "$INPUT" -vf "vidstabdetect" -f null -
+fi
+
+# gdb --args \
 "$FFMPEG" "${INPUT_FLAGS[@]}" -i "$INPUT" "${OUTPUT_FLAGS[@]}" -y "${FLAGS[@]}" "$OUTPUT"
 
