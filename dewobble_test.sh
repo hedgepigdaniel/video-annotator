883b3df0716fa055d51b8640539640ef0fdcc9a9
Daniel Playfair Cal <daniel.playfair.cal@gmail.com>
2021-04-27 23:57:16 +1000

    use a band of set width for the original

--- dewobble_test.sh
+++ dewobble_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -x
+set -x -e
 
 if [ $# -lt 4 ]; then
 	echo "Usage: $0 <opencl filter> <intel|amd|nvidia> <input> <output>"
@@ -30,20 +30,25 @@ if [ $GPU = "intel" ]; then
 elif [ $GPU = "intelcomp" ]; then
 	INPUT_FLAGS=(
 		-init_hw_device vaapi=intel_vaapi:,driver=iHD,kernel_driver=i915
-		-hwaccel vaapi -hwaccel_device intel_vaapi
-		-init_hw_device opencl=intel_opencl@intel_vaapi -filter_hw_device intel_opencl
+		#-hwaccel vaapi -hwaccel_device intel_vaapi
+		-hwaccel nvdec
+		#-init_hw_device opencl=intel_opencl@intel_vaapi -filter_hw_device intel_opencl
+		-init_hw_device opencl=nvidia_opencl:1.0 -filter_hw_device nvidia_opencl
 	)
-	IN_CAMERA="in_p=fish:in_fl=943"
-	OUT_CAMERA="out_p=fish:out_fl=943"
-	THIRD_WIDTH=640
+	IN_CAMERA="in_p=fish:in_fl=1623"
+	OUT_CAMERA="out_p=rect:out_fl=1100"
+	HEIGHT=1520
+	BAND=160
 	SMOOTHING=90
 	OUTPUT_FLAGS=(
 		-filter_complex
-"[0:v]hwupload,dewobble_opencl=$IN_CAMERA:$OUT_CAMERA:stab=none,hwdownload,format=nv12[base];"\
-"[0:v]hwupload,dewobble_opencl=$IN_CAMERA:out_w=$THIRD_WIDTH:$OUT_CAMERA:out_fx=$(($THIRD_WIDTH * 3 / 2)):stab=sg:stab_r=$SMOOTHING,hwdownload,format=nv12,format=yuv420p[left];"\
-"[0:v]vidstabtransform=smoothing=$SMOOTHING:optzoom=0:crop=black,format=nv12,hwupload,dewobble_opencl=$IN_CAMERA:out_w=$THIRD_WIDTH:$OUT_CAMERA:out_fx=$((-$THIRD_WIDTH * 1 / 2)):stab=none,hwdownload,format=nv12,format=yuv420p[right];"\
-"[base][left]overlay[base2];"\
-"[base2][right]overlay=x=$(($THIRD_WIDTH * 2))[out]"
+"[0:v]hwupload,split[hw1][hw2];"\
+"[hw1]dewobble_opencl=$IN_CAMERA:$OUT_CAMERA:stab=none[base];"\
+"[hw2]dewobble_opencl=$IN_CAMERA:out_h=$(($HEIGHT / 2 - $BAND / 2)):$OUT_CAMERA:out_fy=$(( $HEIGHT / 2 )):stab=sg:stab_r=$SMOOTHING[top];"\
+"[0:v]vidstabtransform=smoothing=$SMOOTHING:optzoom=0:crop=black,format=nv12,hwupload,dewobble_opencl=$IN_CAMERA:out_h=$(( $HEIGHT / 2 - $BAND / 2)):$OUT_CAMERA:out_fy=$(( -$BAND / 2)):stab=none[bottom];"\
+"[base][top]overlay_opencl[base2];"\
+"[base2][bottom]overlay_opencl=y=$(( $HEIGHT / 2 + $BAND / 2 ))[outhw];"\
+"[outhw]hwdownload,format=nv12[out]"
 		-map "[out]"
 		-map "0:a"
 		-c:v h264_nvenc -qp 19
